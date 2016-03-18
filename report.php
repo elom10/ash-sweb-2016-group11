52ee20a181fa869011bd203b2d4e155234a64c58
elom10 <ebendzobo@gmail.com>
2016-03-18 12:45:13 +0000

    report problem working
    clicking on button adds report to problem database.

--- report.php
+++ report.php
@@ -6,25 +6,19 @@ include_once("adb.php");
 *Users  class
 */
 class report extends adb{
-	function reports(){
-	}
+
 	/**
 	*Adds a new report
 	*@param string usercode  name
 	*@param string lab lab name
 	*@param string equipment equipment name
 	*/
-	function addReport($usercode,$lab='none',$problem='none',$equipment='none'){
-		$strQuery="UPDATE db_problem set
-						LAB='$lab',
-						PROBLEM='$problem',
-						EQUIPMENT='$equipment',
-						WHERE USERCODE=$usercode";
+	function addReport($lab,$equipment,$concern){
+		$strQuery="insert into problem set
+						LAB='$lab', EQUIPMENT='$equipment',CONCERN='$concern'";
 		return $this->query($strQuery);				
 	}
 	
-	function getAllProblems()
-
 
 }
 ?>
